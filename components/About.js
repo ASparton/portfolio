07e42f8ca41b4e3daea398b9deca5105a8d4df16
Beyl <sparton.alexandre@gmail.com>
2022-03-14 18:00:33 +0100

    about & technos section done + new fonts for title

--- components/About.js
+++ components/About.js
@@ -3,28 +3,39 @@ import Image from 'next/image'
 // images
 import mycoffee from '/public/images/photos/mycoffee.png';
 
+// styles
+import styles from '/styles/components/about.module.css'
+
 function About() {
   return (
     <section name="about">
-      <Image src={mycoffee} alt="Me & my coffee" />
-      <h1>Hello World!</h1>
-      <p>How many times were you frustrated while looking
-        out for a good collection of programming/
-        algorithm/ interview questions? What did you
-        expect and what did you get? This portal has been
-        created to provide well written, well thought and
-        well explained solutions for selected questions.
-        An IIT Roorkee alumnus and founder of
-        GeeksforGeeks. He loves to solve programming
-        problems in most efficient ways. Apart from
-        GeeksforGeeks, he has worked with DE Shaw and
-        Co. as a software developer and JIIT Noida as
-        an assistant professor. It is a good platform
-        to learn programming. It is an educational
-        website. Prepare for the Recruitment drive of
-        product    based companies like Microsoft, Amazon,
-        Adobe etc with a free online placement
-        preparation course.</p>
+      <div className={styles.aboutSection}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.helloWorld}>Hello World!</h1>
+          <p>
+            How many times were you frustrated while looking
+            out for a good collection of programming/
+            algorithm/ interview questions? What did you
+            expect and what did you get? This portal has been
+            created to provide well written, well thought and
+            well explained solutions for selected questions.
+            An IIT Roorkee alumnus and founder of
+            GeeksforGeeks. He loves to solve programming
+            problems in most efficient ways. Apart from
+            GeeksforGeeks, he has worked with DE Shaw and
+            Co. as a software developer and JIIT Noida as
+            an assistant professor. It is a good platform
+            to learn programming. It is an educational
+            website. Prepare for the Recruitment drive of
+            product    based companies like Microsoft, Amazon,
+            Adobe etc with a free online placement
+            preparation course.
+          </p>
+        </div>
+        <div className={styles.coffeeImage}>
+          <Image src={mycoffee} alt="Me & my coffee" />
+        </div>
+      </div>
     </section>
   )
 }
--- components/Technos.js
+++ components/Technos.js
@@ -11,20 +11,48 @@ import pythonLogo from '/public/images/icons/python.png'
 import postgresqlLogo from '/public/images/icons/postgresql.png' 
 import dockerLogo from '/public/images/icons/docker.png' 
 
+// styles
+import styles from '/styles/components/technos.module.css'
+
 function Technos() {
   return (
-    <section name="technos">
-      <h2>Technologies && Tools I like to work on these days</h2>
-      <div>
-        <Image src={jsLogo} alt="Javascript logo" />
-        <Image src={reactLogo} alt="React.js logo" />
-        <Image src={nextLogo} alt="Next.js logo" />
-        <Image src={csharpLogo} alt="C# logo" />
-        <Image src={unityLogo} alt="Unity logo" />
-        <Image src={godotLogo} alt="Godot logo" />
-        <Image src={pythonLogo} alt="Python logo" />
-        <Image src={postgresqlLogo} alt="Postgresql logo" />
-        <Image src={dockerLogo} alt="Docker logo" />
+    <section name="technos" className={styles.technosSection}>
+      <h2>Technologies & Tools</h2>
+      <h2>I like to work on these days</h2>
+      <div className={styles.iconsContainer}>
+        <div className={styles.iconsRow}>
+          <a className={styles.icon} href="https://www.javascript.com/" title="Javascript website" target="_blank" rel="noopener noreferrer">
+            <Image src={jsLogo} alt="Javascript logo" />
+          </a>
+          <a className={styles.icon} href="https://reactjs.org/" title="React JS website" target="_blank" rel="noopener noreferrer">
+            <Image src={reactLogo} alt="React.js logo" />
+          </a>
+          <a className={styles.icon} href="https://nextjs.org/" title="Next JS website" target="_blank" rel="noopener noreferrer">
+            <Image src={nextLogo} alt="Next.js logo" />
+          </a>
+        </div>
+        <div className={styles.iconsRow}>
+          <a className={styles.icon} href="https://docs.microsoft.com/en-us/dotnet/csharp/" title="C# website" target="_blank" rel="noopener noreferrer">
+            <Image src={csharpLogo} alt="C# logo" />
+          </a>
+          <a className={styles.icon} href="https://unity.com/" title="Unity website" target="_blank" rel="noopener noreferrer">
+            <Image src={unityLogo} alt="Unity logo" />
+          </a>
+          <a className={styles.icon} href="https://godotengine.org/" title="Godot website" target="_blank" rel="noopener noreferrer">
+            <Image src={godotLogo} alt="Godot logo" />
+          </a>
+        </div>
+        <div className={styles.iconsRow}>
+          <a className={styles.icon} href="https://www.python.org/" title="Python website" target="_blank" rel="noopener noreferrer">
+            <Image src={pythonLogo} alt="Python logo" />
+          </a>
+          <a className={styles.icon} href="https://www.postgresql.org/" title="Postgresql website" target="_blank" rel="noopener noreferrer">
+            <Image src={postgresqlLogo} alt="Postgresql logo" />
+          </a>
+          <a className={styles.icon} href="https://www.docker.com/" title="Docker website" target="_blank" rel="noopener noreferrer">
+            <Image src={dockerLogo} alt="Docker logo" />
+          </a>
+        </div>
       </div>
     </section>
   )
