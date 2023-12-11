3d2b8f4a74924b52b225e36209310e89171ded2e
= <=>
2023-12-11 14:04:37 +0100

    feat: experiences updated

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -1,38 +1,46 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-import { List } from "@mantine/core";
-import { IconPrompt, IconWorld } from "@tabler/icons";
+import { Image, List } from '@mantine/core';
+import { IconCalendarEvent, IconPrompt, IconWorld } from '@tabler/icons';
 
-import "../../styles/components/sections/experiences.css";
+import '../../styles/components/sections/experiences.css';
 
 export default function Experience({ experience }) {
-  return (
-    <article className="experience">
-      <div>
-        <h1>{experience.title}</h1>
-        <div className="website">
-          <IconWorld />
-          <a
-            href={experience.companyUrl}
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            {experience.companyName}
-          </a>
-        </div>
-        <p>
-          {experience.startDate} - {experience.endDate}
-        </p>
-      </div>
-      <List withPadding icon={<IconPrompt />}>
-        {experience.points.map((point, key) => (
-          <List.Item key={key}>{point}</List.Item>
-        ))}
-      </List>
-    </article>
-  );
+	return (
+		<article className="experience">
+			<div>
+				<h1>{experience.title}</h1>
+				<div className="website">
+					<IconWorld />
+					<a
+						href={experience.companyUrl}
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						{experience.companyName}
+					</a>
+				</div>
+				<div className="date">
+					<IconCalendarEvent width={32} />
+					<p>{experience.startDate} - {experience.endDate}</p>
+				</div>
+			</div>
+			<List withPadding icon={<IconPrompt />}>
+				{experience.points.map((point, key) => (
+					<List.Item key={key}>{point}</List.Item>
+				))}
+			</List>
+			<List className="technos-list" withPadding>
+				{experience.technologiesLogosUrl.map((logoUrl, key) => (
+					<List.Item key={key}>
+						<Image src={logoUrl} />
+					</List.Item>
+				))}
+			</List>
+		</article>
+	);
 }
 
 Experience.propTypes = {
-  experience: PropTypes.object,
+	experience: PropTypes.object,
 };
--- src/experiences.js
+++ src/experiences.js
@@ -1,17 +1,27 @@
 const experiences = [
   {
     sectionName: "HelloWork",
-    title: "Python Developer",
+    title: "Backend Developer",
     companyName: "HelloWork",
     companyUrl: "https://www.hellowork-group.com/en/uk/",
     startDate: "January 2023",
     endDate: "Current position",
     points: [
-      "Python scraping",
-      "Internal scraping tools development",
-      "Data flows management",
-      "Data science",
+      "Head of the scraping project (Scrapy framework)",
+      "Data fetching and flows management",
     ],
+    technologiesLogosUrl: [
+      "https://seeklogo.com/images/P/python-logo-A32636CAA3-seeklogo.com.png",
+      "https://seeklogo.com/images/J/java-logo-7F8B35BAB3-seeklogo.com.png",
+      "https://i0.wp.com/phocode.com/wp-content/uploads/2019/11/scrapyLogo.png?fit=300%2C300&ssl=1",
+      "https://seeklogo.com/images/S/selenium-logo-DB9103D7CF-seeklogo.com.png",
+      "https://seeklogo.com/images/M/mongodb-logo-D13D67C930-seeklogo.com.png",
+      "https://seeklogo.com/images/E/elasticsearch-logo-5C32E9FAEC-seeklogo.com.png",
+      "https://seeklogo.com/images/D/docker-logo-6D6F987702-seeklogo.com.png",
+      "https://seeklogo.com/images/G/gitlab-logo-757620E430-seeklogo.com.png",
+      "https://seeklogo.com/images/J/jenkins-logo-07C99BD83D-seeklogo.com.png",
+      "https://seeklogo.com/images/N/nomad-logo-04537E7E8A-seeklogo.com.png",
+    ]
   },
   {
     sectionName: "Tsukuba",
@@ -21,10 +31,15 @@ const experiences = [
     startDate: "April 2022",
     endDate: "June 2022",
     points: [
-      "Contribution to N-Version Machine learning systems reliability research",
+      "Contributed to N-Version Machine learning systems reliability research",
       "Development & Studies of Machine Learning systems able to recognize images of road signs",
       "Helped improve the current reliability model",
     ],
+    technologiesLogosUrl: [
+      "https://seeklogo.com/images/P/python-logo-A32636CAA3-seeklogo.com.png",
+      "https://seeklogo.com/images/J/jupyter-logo-A91705F539-seeklogo.com.png",
+      "https://seeklogo.com/images/T/tensorflow-logo-02FCED4F98-seeklogo.com.png",
+    ]
   },
 ];
 
